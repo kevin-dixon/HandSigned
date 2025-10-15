4014fb945a83693a0ba7e412cb707f136a620f9f
kevin-dixon <87494457+kevin-dixon@users.noreply.github.com>
2025-10-14 18:12:57 -0600

    initial bones

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// src/App.js
+function App() {
+  return (
+    <div className="App">
+      <h1>My Simple Deployed React Site!</h1>
+      <p>If you see this, the deployment worked.</p>
+    </div>
+  );
+}
+
+export default App;
