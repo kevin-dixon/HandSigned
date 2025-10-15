182300877204c2a13f5ac9bb92f7fc017e20a56c
kevin-dixon <87494457+kevin-dixon@users.noreply.github.com>
2025-10-14 18:33:05 -0600

    updated styles

---
+++ src/Cards.js
@@ -0,0 +1,20 @@
+// src/Cards.js
+
+import React from 'react';
+
+const Cards = () => {
+  return (
+    <div>
+      <h2>Card Collection</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus at neque
+        velit. Sed pretium enim in ante vulputate, sed scelerisque turpis feugiat.
+        Cras ac diam non justo auctor posuere. Suspendisse potenti. Nam eu mi at
+        justo venenatis fermentum a sit amet magna. Curabitur vel leo id urna
+        condimentum luctus.
+      </p>
+    </div>
+  );
+};
+
+export default Cards;
